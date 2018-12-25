e3377066cc91c5eaf988b091d1917f8cb8e1456c
DESKTOP-9KLJ4R7\Anonymous <1728494009@qq.com>
2018-12-25 16:02:04 +0800

    测试引用模块

---
+++ ScriptDemo/module1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+'''
+    自定义的引用测试模块1
+'''
+
+def hello():
+    '''
+        模块函数 hello()
+    '''
+    return 'hello 亡命之徒'
---
+++ ScriptDemo/module2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+'''
+    自定义测试模块1
+'''
+
+def quadrature(num1=0, num2=0):
+    '''
+        自定义求和函数：参数为入两个数字
+    '''
+    return num1 * num2
