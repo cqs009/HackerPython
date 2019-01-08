2878d2abe07aa58559747a385afb05135e696093
DESKTOP-9KLJ4R7\Anonymous <1728494009@qq.com>
2019-01-08 18:11:57 +0800

    文件目录权限操作部分函数使用

--- ScriptDemo/py09day.py
+++ ScriptDemo/py09day.py
@@ -31,12 +31,36 @@ print(ret)
 # os.getcwd() 查看当前的path
 print(os.getcwd())
 
+# os.chdir() 更改文件目录
+os.chdir('testpage')
+print(os.getcwd())
+os.chdir('h:\HackerPython\ScriptDemo')
+print(os.getcwd())
 
+# os.chmod(path,mode) 更改权限
+# mode部分的参数
+# 参数值                权限掩码                说明
+# stat.S_IXOTH          0o001           其他用户的执行权限
+# stat.S_IWOTH          0o002           其他用户的写权限
+# stat.S_IROTH          0o004           其他用户的读权限
+# stat.S_IRWXO          0o007           其他用户的所有权限       
+# stat.S_IXGRP          0o010           用户组的执行权限
+# stat.S_IWGRP          0o020           用户组的写权限
+# stat.S_IRGRP          0o040           用户组的读权限
+# stat.S_IRWXG          0o070           用户组的全部权限
+# stat.S_IXUSR          0o100           拥有者的执行权限
+# stat.S_IWUSR          0o200           拥有着的写权限
+# stat.S_IRUSR          0o400           拥有者的读权限
+#stat.S_ISVTX           0o1000          目录里文件目录只有拥有者才可删除更改
+#stat.S_ISGID           0o2000          执行此文件其进程有效组为文件所在组
+#stat.S_ISUID           0o4000          执行此文件其进程有效用户为文件所有者
+#stat.S_IREAD                           windows下设为只读
+#stat.S_IWRITE                          windows下取消只读
 
+os.chmod('testpage',0o002)
+print('修改成功')
 
-
-
-
+# 更改文件所有者 os.chown(path,uid,gid) 参数：文件路径，所属用户ID，所属用户组ID
 
 
 
